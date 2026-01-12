ca9ef95965e422eec283b553bbd42ef3e45b7aba
Papp Dávid <impappdavid@gmail.com>
2026-01-12 11:06:00 +0100

    Making the 4.4 part

--- bloglist-backend/utils/list_helper.js
+++ bloglist-backend/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+   const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+
+  // If blogs is empty, reduce returns the initial value (0)
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
