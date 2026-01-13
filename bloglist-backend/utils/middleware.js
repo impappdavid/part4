8695014f7c071152996ef775d7f76e880d51a935
Papp Dávid <impappdavid@gmail.com>
2026-01-13 16:51:27 +0100

    Making the 4.20 part

--- bloglist-backend/utils/middleware.js
+++ bloglist-backend/utils/middleware.js
@@ -28,8 +28,22 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    // Attach the token to the request object
+    request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null
+  }
+
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
